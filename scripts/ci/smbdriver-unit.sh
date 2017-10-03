e7b728a8acf0d47e975a6785ea0fa908015f28d1
Paul Warren <paul.warren@emc.com>
2017-10-03 09:17:20 -0700

    use go get instead of go install
    [151410959](https://www.pivotaltracker.com/story/show/151410959)
    
    Signed-off-by: Kai Xiang <kxiang@pivotal.io>

--- scripts/ci/smbdriver-unit.sh
+++ scripts/ci/smbdriver-unit.sh
@@ -10,8 +10,8 @@ export PATH=$GOROOT/bin:$PATH
 export GOPATH=$PWD
 export PATH=$PWD/bin:$PATH
 
-go install github.com/onsi/ginkgo/ginkgo
-go install github.com/onsi/gomega
+go get github.com/onsi/ginkgo/ginkgo
+go get github.com/onsi/gomega
 
 pushd src/github.com/cloudfoundry/smbdriver
   ginkgo -r -keepGoing -p -trace -randomizeAllSpecs -progress --race "$@"
