653a99ed4e89e9868f5aedd23ca1f75403e9e9a3
Kai Xiang <kxiang@pivotal.io>
2018-03-19 14:03:39 -0700

    add unit test for smbdriver in windows
    [#155668387](https://www.pivotaltracker.com/story/show/155668387)

---
+++ scripts/ci/smbdriver-unit.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = “Stop”;
+trap { $host.SetShouldExit(1) }
+
+cd smb-volume-release
+
+$env:GOPATH=$PWD
+$env:PATH="$PWD/bin;$env:PATH"
+
+go install github.com/onsi/ginkgo/ginkgo
+
+cd src/code.cloudfoundry.org/smbdriver
+ginkgo -r -keepGoing -p -trace -randomizeAllSpecs -progress --race
+
