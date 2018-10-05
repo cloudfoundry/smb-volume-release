96b33edfe9a7a9b65b4c5725c9fc586f1c34bd9e
Dave Walter <dwalter@pivotal.io>
2018-10-05 16:37:11 -0700

    Rename azurefilebroker-unit to run-submodule-unit
    - it is now a generic script that can run the unit tests of any
    Linux submodule
    - the desired submodule is specified via the SUBMODULE_NAME param
    
    [#160431033](https://www.pivotaltracker.com/story/show/160431033)
    
    Co-authored-by: Maria Shaldibina <mshaldibina@pivotal.io>

--- scripts/ci/run-submodule-unit.sh
+++ scripts/ci/run-submodule-unit.sh
@@ -14,6 +14,6 @@ go get github.com/onsi/ginkgo/ginkgo
 go get github.com/onsi/gomega
 go get gopkg.in/DATA-DOG/go-sqlmock.v1
 
-pushd src/code.cloudfoundry.org/azurefilebroker
+pushd src/code.cloudfoundry.org/${SUBMODULE_NAME}
   ginkgo -r -keepGoing -p -trace -randomizeAllSpecs -progress --race "$@"
 popd
