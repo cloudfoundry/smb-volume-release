b90bdeb835e51c45d29b595326b5cc7b22c7fdae
Dennis Leon <dleon@pivotal.io>
2019-06-24 10:52:06 -0700

    Stop changing ownership of child cell mount directories
    [#166854567] https://www.pivotaltracker.com/story/show/166854567

--- src/bosh_release/bosh_release_test.go
+++ src/bosh_release/bosh_release_test.go
@@ -3,6 +3,7 @@ package bosh_release_test
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
 	"github.com/onsi/gomega/gexec"
 	"os/exec"
 )
@@ -16,6 +17,46 @@ var _ = Describe("BoshReleaseTest", func() {
 		state := findProcessState("smbdriver")
 
 		Expect(state).To(Equal("running"))
+	})
+
+	Context("Monit restart", func() {
+		JustBeforeEach(func() {
+			cmd := exec.Command("bosh", "-d", "bosh_release_test", "restart", "smbdriver", "-n")
+			session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+			Expect(err).NotTo(HaveOccurred())
+			Eventually(session).Should(gexec.Exit(0))
+		})
+
+		Context("smb mount path has a child directory", func() {
+			BeforeEach(func() {
+				By("sudo touch /var/vcap/data/volumes/smb/child_directory")
+				cmd := exec.Command("bosh", "-d", "bosh_release_test", "ssh", "-c", "sudo touch /var/vcap/data/volumes/smb/child_directory")
+				session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+				Expect(err).NotTo(HaveOccurred())
+				Eventually(session).Should(gexec.Exit(0), string(session.Out.Contents()))
+
+				By("sudo chown root:root /var/vcap/data/volumes/smb/child_directory")
+				cmd = exec.Command("bosh", "-d", "bosh_release_test", "ssh", "-c", "sudo chown root:root /var/vcap/data/volumes/smb/child_directory")
+				session, err = gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+				Expect(err).NotTo(HaveOccurred())
+				Eventually(session).Should(gexec.Exit(0), string(session.Out.Contents()))
+			})
+
+			It("Only cell_mount_path should chown and not any child directories", func() {
+				cmd := exec.Command("bosh", "-d", "bosh_release_test", "ssh", "-c", "sudo stat --format='%U:%G' /var/vcap/data/volumes/smb")
+				session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+				Expect(err).NotTo(HaveOccurred())
+				Eventually(session).Should(gexec.Exit(0), string(session.Out.Contents()))
+				Expect(session).Should(gbytes.Say("vcap:vcap"))
+
+
+				cmd = exec.Command("bosh", "-d", "bosh_release_test", "ssh", "-c", "sudo stat --format='%U:%G' /var/vcap/data/volumes/smb/child_directory")
+				session, err = gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+				Expect(err).NotTo(HaveOccurred())
+				Eventually(session).Should(gexec.Exit(0), string(session.Out.Contents()))
+				Expect(session).Should(gbytes.Say("root:root"))
+			})
+		})
 	})
 
 	Context("when smbdriver is disabled", func() {
