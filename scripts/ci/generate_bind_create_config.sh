aa44c69822131e35f41a7f47a0ec9b62a3a267b2
Dennis Leon <dleon@pivotal.io>
2019-09-27 10:55:41 -0700

    Add the 'domain' mount option when running PATs
    [#168679324](https://www.pivotaltracker.com/story/show/168679324)
    
    Signed-off-by: Paul Warren <paul.warren@emc.com>

--- scripts/ci/generate_bind_create_config.sh
+++ scripts/ci/generate_bind_create_config.sh
@@ -13,6 +13,6 @@ BIND_CONFIG=${PWD}/bind-create-config/bind-config.json
 
 echo "{\"share\":\"$SMB_REMOTE_PATH\"}" > "${CREATE_CONFIG}"
 
-json_payload1=$(echo "{\"username\":\"$SMB_USERNAME\",\"password\":\"$SMB_PASSWORD\"}" | sed 's/"/\\"/g')
-json_payload2=$(echo "{\"username\":\"$SMB_USERNAME\",\"password\":\"$SMB_PASSWORD\", \"mount\": \"/var/vcap/data/foo\"}" | sed 's/"/\\"/g')
+json_payload1=$(echo "{\"username\":\"$SMB_USERNAME\",\"password\":\"$SMB_PASSWORD\", \"domain\": \"foo\"}" | sed 's/"/\\"/g')
+json_payload2=$(echo "{\"username\":\"$SMB_USERNAME\",\"password\":\"$SMB_PASSWORD\", \"mount\": \"/var/vcap/data/foo\", \"domain\": \"foo\"}" | sed 's/"/\\"/g')
 echo "[\"$json_payload1\", \"$json_payload2\"]" > "${BIND_CONFIG}"
