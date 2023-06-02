f780d71987a57497799dffd6466089dfd0d2f42d
Diego Lemos <diego@diegolemos.net>
2023-06-02 17:06:24 +0100

    Fix possible log cred leak (#442) (#124)
    * Fix possible log cred leak
    
    [#184798214]
    
    * Fix possible log leak of CF password
    
    On 2023-03-27 we discovered that new stemcells were using OS audit tools to
    log every command run on a VM. This was causing CF authentication commands
    of the form `cf auth --client-credentials USERNAME PASSWORD` to be logged
    in plaintext to syslog.
    
    Internal shell commands like setting variables are not logged by OS audit
    tools, so this commit changes our auth from passing a password as an
    argument to passing a password as an environment variable
    
    when a binary is executed on linux the audit system will log the
    execution into syslog. This log includes the arguments provided to the
    binary call. In this script these arguments contain the admin creds for
    the created service broker.
    
    To avoid this, switch to use echo (a bash builtin) and
    <() process substition which in turn provides file descriptors. This can
    then be used with cf curl via the -d param which accepts a path via @
    annotation.
    
    The cf cli currently doesn't support sourcing values from the environment
    or config files for this call. Even once it supports it, we cannot be
    totally sure that our release is running with the required cf cli
    version available. We assume that the cf cli is present on the VM that
    this errand is executed on, but we do not provide it via our release.
    
    This implementation uses cf curl instead.
    This way the data can be provided through means that will not log
    cleartext credentials into syslog.
    
    additionally the smbbroker start script has been updated to not pass the
    creds as flags to the binary. The creds are now set as ENV Vars via the
    app manifest and should not be visible in the app logs anymore either.
    
    Co-authored-by: nouseforaname <34882943+nouseforaname@users.noreply.github.com>

--- spec/jobs/smbbrokerpush/start_sh_spec.rb
+++ spec/jobs/smbbrokerpush/start_sh_spec.rb
@@ -5,6 +5,37 @@ describe 'smbbrokerpush job' do
   let(:release) { Bosh::Template::Test::ReleaseDir.new(File.join(File.dirname(__FILE__), '../../..')) }
   let(:job) { release.job('smbbrokerpush') }
 
+  describe 'app manifest' do 
+    let(:template) { job.template('manifest.yml') }
+    context 'when fully configured with all required credhub and log properties' do
+      let(:manifest_properties) do
+        {
+            "username" => "admin",
+            "password" => "pass",
+            "app_name" => "broker",
+            "app_domain" => "broker",
+            "memory_in_mb" => "1024",
+            "credhub" => {
+                "url" => "some-credhub-url",
+                "uaa_client_id" => "client-id",
+                "uaa_client_secret" => "client-secret",
+                "store_id" => "some-store-id",
+            },
+            "log_level" => "some-log-level",
+            "log_time_format" => "some-log-time-format",
+        }
+      end
+
+      it 'successfully renders the yml' do
+        tpl_output = template.render(manifest_properties)
+
+        expect(tpl_output).to include("UAA_CLIENT_ID: \"client-id\"")
+        expect(tpl_output).to include("UAA_CLIENT_SECRET: \"client-secret\"")
+      end
+    end
+
+
+  end
   describe 'start.sh' do
     let(:template) { job.template('start.sh') }
 
@@ -28,8 +59,8 @@ describe 'smbbrokerpush job' do
         expect(tpl_output).to include("bin/smbbroker --listenAddr=\"0.0.0.0:$PORT\"")
         expect(tpl_output).to include("--servicesConfig=\"./services.json\"")
         expect(tpl_output).to include("--credhubURL=\"some-credhub-url\"")
-        expect(tpl_output).to include("--uaaClientID=\"client-id\"")
-        expect(tpl_output).to include("--uaaClientSecret=\"client-secret\"")
+        expect(tpl_output).not_to include("--uaaClientID=\"client-id\"")
+        expect(tpl_output).not_to include("--uaaClientSecret=\"client-secret\"")
         expect(tpl_output).to include("--storeID=\"some-store-id\"")
         expect(tpl_output).to include("--logLevel=\"some-log-level\"")
         expect(tpl_output).to include("--timeFormat=\"some-log-time-format\"")
@@ -48,12 +79,12 @@ describe 'smbbrokerpush job' do
         }
       end
 
-      it 'includes the credhub flags in the script' do
+      it 'includes the non sensitive credhub flags in the script' do
         tpl_output = template.render(manifest_properties)
 
         expect(tpl_output).to include("--credhubURL=\"some-credhub-url\"")
-        expect(tpl_output).to include("--uaaClientID=\"some-uaa-client-id\"")
-        expect(tpl_output).to include("--uaaClientSecret=\"some-uaa-client-secret\"")
+        expect(tpl_output).not_to include("--uaaClientID=\"some-uaa-client-id\"")
+        expect(tpl_output).not_to include("--uaaClientSecret=\"some-uaa-client-secret\"")
         expect(tpl_output).to include("--storeID=\"some-store-id\"")
       end
     end
